1d372377611916dcdce95dffde5a1b27d1f7fdad
asymmentric <74967082+Asymmentric@users.noreply.github.com>
2021-06-23 18:03:15 +0000

    .

--- Program-3.c
+++ Program-3.c
@@ -9,6 +9,6 @@ int main() {
   // Calculating product
   product=a*b;
   // Result up to 2 decimal point is displayed using %.2lf
-  printf("Product = %lf", product);
+  printf("Product = %.2lf", product);
 return 0;   // correct the code
 }
