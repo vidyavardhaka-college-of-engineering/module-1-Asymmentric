683c94fc46ecc1abb05e56a67df337d66906256d
asymmentric <74967082+Asymmentric@users.noreply.github.com>
2021-06-23 18:15:10 +0000

    ok

--- Program-4.c
+++ Program-4.c
@@ -13,6 +13,6 @@ int main() {
     remainder = dividend%divisor; // correct the code
 
     printf("Quotient = %d ", quotient);  // correct the code
-    printf("Remainder = %d ", remainder);  // correct the code
+    printf("Remainder = %d", remainder);  // correct the code
     return 0;
 }
